cf68a10dd8b5760f3bd0e5996af98acc70244374
jtmonroe <jtmonroe@email.wm.edu>
2018-11-14 14:54:26 -0500

    Fixed TOL definitionin Complex.h

--- Complex.h
+++ Complex.h
@@ -1,15 +1,16 @@
 #pragma once
+
+#define TOL 0.000000000000001
+
 #include <iostream>
 #include <string>
 
-//template <class T>
 namespace ComplexMath {
 
 template <class T> class Complex
 {
 public:
     inline Complex(T real = 0.0, T imag = 0.0) {m_real = real; m_imag = imag;}
-
     inline Complex<T> operator+(const Complex<T>& z){return Complex(this->m_real + z.m_real, this->m_imag + z.m_imag);}
     inline Complex<T> operator+(const T& c){return Complex(this->m_real + c, m_imag);}
     inline Complex<T> operator*(const T& c){return Complex(m_real*c, m_imag*c);}
@@ -17,7 +18,7 @@ public:
     inline Complex<T>& operator+=(const Complex<T>& z){this->m_real += z.m_real; this->m_imag += z.m_imag; return *this;}
 
     //inline bool operator==(const Complex<T>& z){return (m_real == z.m_real && m_imag == z.m_imag);}
-    inline bool operator==(const Complex<T>& z){return ((m_real - z.m_real > 0.00001 && m_real - z.m_real < -0.00001) && (m_imag - z.m_imag > 0.00001 && m_imag - z.m_imag < -0.00001));}
+    inline bool operator==(const Complex<T>& z){return ((m_real - z.m_real > TOL && m_real - z.m_real < TOL) && (m_imag - z.m_imag > TOL && m_imag - z.m_imag < TOL));}
     inline bool operator>(const Complex<T>& z){return (m_real*m_real + m_imag*m_imag > z.m_real*z.m_real + z.m_imag*z.m_imag);}
     inline bool operator>(const double& c){return (m_real*m_real + m_imag*m_imag > c);}
 
--- Quantum.h
+++ Quantum.h
@@ -50,6 +50,4 @@ CplexMat sigma_(enum Pauli in)
     }
 }
 
-
-
 }
